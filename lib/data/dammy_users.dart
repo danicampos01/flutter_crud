3cd1feea689c5d0c50bcf54b56130ce01e7b1730
Daniel Campos <danicampos01@gmail.com>
2020-09-19 21:06:02 -0300

    first commit

---
+++ lib/data/dammy_users.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@ploma.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/04/04/12/28/elephant-3289662_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Raimunda',
+    email: 'raimunda@ploma.com.br',
+    avatarUrl: 'https://media.istockphoto.com/vectors/bee-vector-id472301353',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Antonia',
+    email: 'antonio@ploma.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/06/03/01/15/redfish-3449603_960_720.png',
+  )
+};
